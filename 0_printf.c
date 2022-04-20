1e9d4fa51291b7d67cdbf1336be71556a06ca609
root <root@DESKTOP-VB97GVI.localdomain>
2022-04-20 22:02:40 +0200

    correction

---
+++ 0_printf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+**_print - produces output according to a format.
+*@format:it the argument
+*description: produces output according to a format.
+*Return:  always 0
+*/
+int _printf(const char *format, ...)
+{
+va_list arg;
+int done;
+va_start (arg, format);
+done = vfprintf (stdout, format, arg);
+va_end (arg);
+return done;
+}
